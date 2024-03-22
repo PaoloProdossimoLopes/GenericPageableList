32205bca12097a9b2a97640ce53691ee1bb81d6e
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2024-03-22 16:44:31 -0300

    Initial Commit

---
+++ GenericList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GenericList
+//
+//  Created by Paolo Prodossimo Lopes on 22/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
